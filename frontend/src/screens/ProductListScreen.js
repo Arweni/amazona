7113bfed59073a582c40640b546904a682b675c6
arweni <chad5059x1@yahoo.com>
2023-11-20 17:46:20 -0500

    change remove to deleteOne due to deprecation (mongoose)

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -127,7 +127,7 @@ export default function ProductListScreen() {
         toast.success('product deleted successfully');
         dispatch({ type: 'DELETE_SUCCESS' });
       } catch (err) {
-        toast.error(getError(error));
+        toast.error(getError(err));
         dispatch({
           type: 'DELETE_FAIL',
         });
@@ -141,9 +141,9 @@ export default function ProductListScreen() {
         <Col>
           <h1>Products</h1>
         </Col>
-        <Col className="col text-end">
+        <Col className='col text-end'>
           <div>
-            <Button type="button" onClick={createHandler}>
+            <Button type='button' onClick={createHandler}>
               Create Product
             </Button>
           </div>
@@ -156,10 +156,10 @@ export default function ProductListScreen() {
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
+        <MessageBox variant='danger'>{error}</MessageBox>
       ) : (
         <>
-          <table className="table">
+          <table className='table'>
             <thead>
               <tr>
                 <th>ID</th>
@@ -180,16 +180,16 @@ export default function ProductListScreen() {
                   <td>{product.brand}</td>
                   <td>
                     <Button
-                      type="button"
-                      variant="light"
+                      type='button'
+                      variant='light'
                       onClick={() => navigate(`/admin/product/${product._id}`)}
                     >
                       Edit
                     </Button>
                     &nbsp;
                     <Button
-                      type="button"
-                      variant="light"
+                      type='button'
+                      variant='light'
                       onClick={() => deleteHandler(product)}
                     >
                       Delete
